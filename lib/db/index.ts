00399bc53c17fa89edfdbf6803e8612ffd2faef2
samadhi1311 <samadhilakmina.sl@gmail.com>
2024-12-05 16:44:42 +0530

    Updated database client configuration

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -1,18 +1,17 @@
-import 'dotenv/config';
 import * as schema from './schema';
 import { drizzle } from 'drizzle-orm/libsql';
 import { createClient } from '@libsql/client';
 
-const url = process.env.TURSO_DATABASE_URL;
-const authToken = process.env.TURSO_AUTH_TOKEN;
+const url = process.env.NEXT_PUBLIC_TURSO_DATABASE_URL!;
+const authToken = process.env.NEXT_PUBLIC_TURSO_AUTH_TOKEN!;
 
 if (!url || !authToken) {
 	throw new Error('Missing TURSO_DATABASE_URL or TURSO_AUTH_TOKEN environment variable');
 }
 
 const turso = createClient({
-	url: url,
-	authToken: authToken,
+	url: process.env.NEXT_PUBLIC_TURSO_DATABASE_URL!,
+	authToken: process.env.NEXT_PUBLIC_TURSO_AUTH_TOKEN,
 });
 
 const db = drizzle(turso, { schema });
